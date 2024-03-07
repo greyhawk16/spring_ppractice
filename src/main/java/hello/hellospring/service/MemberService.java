9b2d6178cc23e1b383d244452a2242f59137e8c4
양준헌 <greyhawk16@BlackStar-ui-MacBookPro.local>
2024-03-07 09:42:43 +0900

    회원 서비스 테스트케이스 생성

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -8,7 +8,13 @@ import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    private final MemberRepository memberRepository;
+
+
 
     /**
      * 회원가입
